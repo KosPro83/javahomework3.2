315261c205b36f68c0a69b3c9695221df713bf6f
KosPro <Pronin.k.a@bk.ru>
2024-03-11 14:46:19 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 98; // вес в килограммах
+        double weight = 98; // вес в килограммах
         double height = 1.87; // рост в метрах
         double bmi = service.calculate(weight, height); // считаем ИМТ
         String bmiResult = String.format("%.1f", bmi); // округляем
