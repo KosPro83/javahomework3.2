681cc36074eec68f1bc0ca07308c7d18e0fba901
KosPro <Pronin.k.a@bk.ru>
2024-03-14 16:38:56 +0300

    .

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int CalcService(int weight, double height) {
+    public int calculate(int weight, double height) {
         double bmi = weight / (height * height); // считаем ИМТ
         return (int) bmi; // возвращаем рассчитанный индекс массы тела
     }
